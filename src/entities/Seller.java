07155bd7d4603893492deaabd956428639165ec7
Gabriel Silva <gabiles278@gmail.com>
2023-07-13 22:30:08 -0300

    created Department and Seller classes

---
+++ src/entities/Seller.java
@@ -0,0 +1,96 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class Seller {
+    private Integer id;
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+    private Double baseSalary;
+    private Department department;
+    
+    public Seller() {
+        
+    }
+
+    public Seller(Integer id, String name, String email, LocalDate birthDate, Double baseSalary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    /**
+     * @return Integer return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return LocalDate return the birthDate
+     */
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    /**
+     * @param birthDate the birthDate to set
+     */
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    /**
+     * @return Double return the baseSalary
+     */
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    /**
+     * @param baseSalary the baseSalary to set
+     */
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+}
