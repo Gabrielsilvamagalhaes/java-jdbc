ecacb262ed304217de0013c5fedd9262702c9c56
Gabriel Silva <gabiles278@gmail.com>
2023-07-13 22:18:06 -0300

    Created exceptions and connection

---
+++ src/db/Connect.java
@@ -0,0 +1,80 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Connect {
+    private static final String url = "jdbc:mysql://localhost:3306/coursejdbc";
+    private static final String user = "root";
+    private static final String password = "59710826";
+
+    private static Connection conn;
+
+    public static Connection getConexao() {
+        try {
+            if (conn == null) {
+                conn = DriverManager.getConnection(url, user, password);
+                return conn;
+
+            } else {
+                return conn;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void close(ResultSet rs, Statement st) {
+        if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+
+        if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+
+    }
+
+    public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+    public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+    
+}
